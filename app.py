e0ddf0e8a16a350ad1a7d6245854a9ae28bfc428
bushrashahzadd2002-bit <bushrashahzadd2002@gmail.com>
2025-11-21 17:18:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ st.write("Paste LinkedIn profile URLs below (one per line):")
 
 urls_input = st.text_area("LinkedIn URLs", height=200)
 n8n_webhook_url = st.secrets["WEBHOOK_URL"]
-N8N_USER = st.secrets["N8N_USER"]
-N8N_PASSWORD = st.secrets["N8N_PASSWORD"]
+#N8N_USER = st.secrets["N8N_USER"]
+#N8N_PASSWORD = st.secrets["N8N_PASSWORD"]
 if st.button("Run Lead Scoring"):
     if not urls_input.strip():
         st.error("Please enter at least one link.")
@@ -22,11 +22,11 @@ if st.button("Run Lead Scoring"):
 
         with st.spinner("Processing leads..."):
             try:
-                response = requests.post(
+               response = requests.post(
                         n8n_webhook_url,
-                        json={"urls": urls,
-                             auth: (N8N_USER,N8N_PASSWORD)}
-                )
+                        json={"urls": urls},
+                        auth=(st.secrets["N8N_USER"], st.secrets["N8N_PASSWORD"])
+                    )
 
                 data = response.json()
 
