ec012ad63aae99b40232cc59cfd7e55e97dd74ab
bushrashahzadd2002-bit <bushrashahzadd2002@gmail.com>
2025-11-21 12:34:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import requests
 import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
 
 st.set_page_config(page_title="Lead Scoring", layout="wide")
 
@@ -11,6 +13,20 @@ st.write("Paste LinkedIn profile URLs below (one per line):")
 urls_input = st.text_area("LinkedIn URLs", height=200)
 
 n8n_webhook_url = "https://oversolemnly-vanitied-milagro.ngrok-free.dev/webhook/d71f69a1-6e6a-40d8-b510-4be5c02323f6"
+
+SHEET_ID = "1Rb18WIcgrHWEncA_ZMtdUsmXP4VepQ5cPqg88mgtgWI"
+SHEET_NAME = "Sheet1"
+
+def append_to_google_sheet(df):
+    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+    creds = Credentials.from_service_account_file("service_account.json", scopes=scopes)
+    client = gspread.authorize(creds)
+
+    sheet = client.open_by_key(SHEET_ID).worksheet(SHEET_NAME)
+
+    rows = [df.columns.tolist()] + df.values.tolist()
+
+    sheet.append_rows(rows, value_input_option="USER_ENTERED")
 
 if st.button("Run Lead Scoring"):
     if not urls_input.strip():
@@ -32,17 +48,11 @@ if st.button("Run Lead Scoring"):
 
                     df = pd.DataFrame(data)
 
-                    st.success("Lead scoring completed!")
-
+                    st.success("Lead Scoring completed!")
                     st.dataframe(df)
 
-                    csv = df.to_csv(index=False)
-                    st.download_button(
-                        "Download CSV",
-                        csv,
-                        "leads.csv",
-                        "text/csv"
-                    )
+                    append_to_google_sheet(df)
+                    st.success("Data added to Google Sheet!")
 
             except Exception as e:
                 st.error(f"Error: {e}")
